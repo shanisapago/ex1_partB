ac9d187c7d84ad3009efa4b50ca57b8b10c3f0ba
shanisapago <shani.sapago@gmail.com>
2022-12-14 21:50:03 +0200

    changes

--- Distance.h
+++ Distance.h
@@ -2,17 +2,20 @@
 #define DISTANCE_H_
 #include <vector>
 using namespace std;
+//abstract class of distance, has 2 vectors
 class Distance{
     protected:
     vector<double> vec1;
     vector<double> vec2;
 
     public:
+    //abstract function that return the distance
     virtual double getDistance()=0;
+    //set first vector
     void setVec1(vector<double> v);
+    //set second vector
     void setVec2(vector<double> v);
-    void printVec1();
-    void printVec2();
+    
     
    
 };
